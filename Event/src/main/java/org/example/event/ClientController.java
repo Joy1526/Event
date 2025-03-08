12935a842bcc8b9c68cc40e8d2d2988f87c14315
ajmainn11 <202314033@student.mist.ac.bd>
2025-03-08 23:55:43 +0600

    Client UI

---
+++ Event/src/main/java/org/example/event/ClientController.java
@@ -0,0 +1,18 @@
+package org.example.event;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ClientController {
+    @FXML
+    Button CreateEvent;
+
+
+
+    public void createEventfunc(ActionEvent event) {
+
+    }
+
+
+}
